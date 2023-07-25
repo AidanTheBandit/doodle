2cc583457ffc1ac110f7dc897a94e794603ea6e3
AidanTheBandit <aidanthebandit@icloud.com>
2023-07-25 18:25:45 -0400

    I didnt think a drawing site would be so hard

--- src/components/SharedWhiteboard.vue
+++ src/components/SharedWhiteboard.vue
@@ -1,29 +1,31 @@
 <template>
-  <div>
+  <div style="overflow: hidden; width: 100vw; height: 100vh;">
+    <div :style="{ width: canvasSize + 'px', height: canvasSize + 'px', position: 'relative' }">
+      <canvas
+        ref="canvas"
+        @mousedown="startDrawing"
+        @mousemove="draw"
+        @mouseup="stopDrawing"
+        @mouseleave="stopDrawing"
+        v-on:touchstart.prevent.stop="onTouchStart"
+        v-on:touchmove.prevent.stop="onTouchMove"
+        v-on:touchend="onTouchEnd"
+        @contextmenu.prevent="startPanning"
+        :style="{ cursor: eraser ? 'default' : 'crosshair', touchAction: 'none', transform: `translate(${offsetX}px, ${offsetY}px)` }"
+      ></canvas>
+    </div>
+
     <div class="toolbar" id="toolbar">
       <button
         v-for="size in [1, 2, 5, 10, 20]"
         :key="size"
         @click="penSize = size"
         :class="{ active: !eraser && penSize === size }"
-        class="pen-size-btn"
       >
         {{ size }}
       </button>
-      <button
-        @click="eraser = !eraser"
-        :class="{ active: eraser }"
-        class="eraser-btn"
-      >
-        Eraser
-      </button>
+      <button @click="eraser = !eraser" :class="{ active: eraser }">Eraser</button>
     </div>
-    <canvas
-      ref="canvas"
-      @mousedown="startDrawing"
-      @mousemove="draw"
-      @mouseup="stopDrawing"
-    ></canvas>
   </div>
 </template>
 
@@ -38,29 +40,54 @@ export default {
       context: null,
       drawing: false,
       penSize: 1,
-      startX: 0,
-      startY: 0,
       eraser: false,
-      initialLoad: true
+      initialLoad: true,
+      lastX: 0,
+      lastY: 0,
+      offsetX: 0,
+      offsetY: 0,
+      startOffsetX: 0,
+      startOffsetY: 0,
+      panning: false,
+      touchStartDistance: 0,
+      touchStartOffsetX: 0,
+      touchStartOffsetY: 0,
+      canvasSize: window.innerWidth < 768 ? 2000 : 4000, // Adjust the canvas size for mobile devices
     };
   },
   methods: {
+    getMousePos(e) {
+      const rect = this.canvas.getBoundingClientRect();
+      const clientX = e.type.startsWith('touch') ? e.touches[0].clientX : e.clientX;
+      const clientY = e.type.startsWith('touch') ? e.touches[0].clientY : e.clientY;
+      return {
+        x: clientX - rect.left,
+        y: clientY - rect.top,
+      };
+    },
     startDrawing(e) {
+      if (e.touches && e.touches.length === 2) {
+        this.startPanning(e);
+        return;
+      }
+
+      if (e.button === 2 || this.panning) return;
       this.drawing = true;
-      this.startX = e.clientX;
-      this.startY = e.clientY - 30;
+      const { x, y } = this.getMousePos(e);
+      this.lastX = x;
+      this.lastY = y;
+      this.drawLine(x, y, x, y, this.penSize, true, this.eraser);
     },
     draw(e) {
       if (!this.drawing) return;
-      const endX = e.clientX;
-      const endY = e.clientY - 30;
-      this.drawLine(this.startX, this.startY, endX, endY, this.penSize, true, this.eraser);
-      this.startX = endX;
-      this.startY = endY;
+      const { x, y } = this.getMousePos(e);
+      this.drawLine(this.lastX, this.lastY, x, y, this.penSize, true, this.eraser);
+      this.lastX = x;
+      this.lastY = y;
     },
     stopDrawing() {
-      if (!this.drawing) return;
       this.drawing = false;
+      this.panning = false;
     },
     drawLine(startX, startY, endX, endY, penSize, emit = true, isEraser = false) {
       this.context.globalCompositeOperation = isEraser ? 'destination-out' : 'source-over';
@@ -71,90 +98,130 @@ export default {
       this.context.lineTo(endX, endY);
       this.context.stroke();
       this.context.closePath();
+
       if (!emit) {
         return;
       }
+
       this.socket.emit('draw', {
-        startX: startX,
-        startY: startY,
-        endX: endX,
-        endY: endY,
-        penSize: penSize,
-        isEraser: isEraser
+        startX,
+        startY,
+        endX,
+        endY,
+        penSize,
+        isEraser,
       });
-    }
+    },
+    startPanning(e) {
+      if ((e.touches && e.touches.length === 2) || e.button === 2) {
+        this.panning = true;
+        this.drawing = false;
+        if (e.touches && e.touches.length === 2) {
+          const dx = e.touches[0].clientX - e.touches[1].clientX;
+          const dy = e.touches[0].clientY - e.touches[1].clientY;
+          this.touchStartDistance = Math.sqrt(dx * dx + dy * dy);
+          this.touchStartOffsetX = this.offsetX;
+          this.touchStartOffsetY = this.offsetY;
+        } else {
+          this.startOffsetX = this.offsetX;
+          this.startOffsetY = this.offsetY;
+          this.lastX = e.touches ? (e.touches[0].clientX + e.touches[1].clientX) / 2 : e.clientX;
+          this.lastY = e.touches ? (e.touches[0].clientY + e.touches[1].clientY) / 2 : e.clientY;
+        }
+      }
+    },
+    pan(e) {
+      if (!this.panning || (e.touches && e.touches.length < 2)) return;
+      const x = e.touches ? (e.touches[0].clientX + e.touches[1].clientX) / 2 : e.clientX;
+      const y = e.touches ? (e.touches[0].clientY + e.touches[1].clientY) / 2 : e.clientY;
+      const offsetX = this.startOffsetX + (x - this.lastX);
+      const offsetY = this.startOffsetY + (y - this.lastY);
+      const maxOffsetX = Math.max(0, this.canvasSize - window.innerWidth);
+      const maxOffsetY = Math.max(0, this.canvasSize - window.innerHeight);
+      this.offsetX = Math.min(0, Math.min(maxOffsetX, offsetX));
+      this.offsetY = Math.min(0, Math.min(maxOffsetY, offsetY));
+    },
+    onTouchStart(e) {
+      if (e.touches.length === 2) {
+        this.startPanning(e);
+      } else {
+        this.startDrawing(e);
+      }
+    },
+    onTouchMove(e) {
+      if (e.touches.length === 2) {
+        this.pan(e);
+      } else {
+        this.draw(e);
+      }
+    },
+    onTouchEnd() {
+      this.stopDrawing();
+    },
   },
   mounted() {
     this.socket = io('https://doodle-api.barkle.chat');
     this.socket.on('draw', (data) => {
       this.drawLine(data.startX, data.startY, data.endX, data.endY, data.penSize, false, data.isEraser);
     });
+
     if (this.initialLoad) {
       this.socket.on('load', (data) => {
-        for (let line of data) {
+        for (const line of data) {
           this.drawLine(line.startX, line.startY, line.endX, line.endY, line.penSize, false, line.isEraser);
         }
         this.initialLoad = false;
       });
       this.socket.emit('load');
     }
+
     this.canvas = this.$refs.canvas;
     this.context = this.canvas.getContext('2d');
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+    this.canvas.width = this.canvasSize;
+    this.canvas.height = this.canvasSize;
+
     window.addEventListener('resize', () => {
-      this.canvas.width = window.innerWidth;
-      this.canvas.height = window.innerHeight;
+      this.canvasSize = window.innerWidth < 768 ? 2000 : 4000;
+      this.canvas.width = this.canvasSize;
+      this.canvas.height = this.canvasSize;
     });
-  }
+
+    // Remove the previous event listener to avoid duplicate touchmove events
+    this.canvas.removeEventListener('touchmove', this.onTouchMove);
+    this.canvas.addEventListener('touchmove', this.onTouchMove, { passive: false });
+  },
 };
 </script>
 
 <style scoped>
-#toolbar {
+canvas {
   position: absolute;
   top: 0;
   left: 0;
-  display: flex;
+  z-index: 1;
+  
+}
+.toolbar {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  z-index: 2;
+  background-color: #f0f0f0;
   padding: 10px;
-  background-color: #f1f1f1;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
 }
-
-.pen-size-btn {
-  cursor: pointer;
+button {
   border: none;
-  background-color: transparent;
-  color: #333;
-  font-size: 16px;
-  padding: 8px 12px;
-  margin-right: 10px;
-  border-radius: 8px;
+  padding: 10px;
+  border-radius: 5px;
+  background: #ddd;
+  font-size: 1rem;
 }
-
-.pen-size-btn.active {
-  background-color: #3490dc;
-  color: #fff;
-}
-
-.eraser-btn {
-  cursor: pointer;
-  border: none;
-  background-color: transparent;
-  color: #333;
-  font-size: 16px;
-  padding: 8px 12px;
-  border-radius: 8px;
-}
-
-.eraser-btn.active {
-  background-color: #3490dc;
-  color: #fff;
-}
-
-canvas {
-  position: absolute;
-  top: 40px;
-  left: 0;
-  border: 1px solid #ccc;
+button.active {
+  background-color: #bbb;
 }
 </style>
